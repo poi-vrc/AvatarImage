173f34f6bb1fafed5d892581541983dba7d6b827
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2021-05-13 20:17:38 +0800

    Add avatar image scripts

---
+++ Assets/chocopoi/AvatarImage/Editor/AvatarImageEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AvatarImageScript))]
+public class AvatarImageEditor : Editor
+{
+    private Texture previewRenderTexture;
+
+    private Camera previewCamera;
+
+    void Awake()
+    {
+        GameObject previewCameraObject = GameObject.Find("AvatarImagePreviewCamera");
+        previewCamera = previewCameraObject.GetComponent<Camera>();
+
+        previewRenderTexture = previewCamera.targetTexture;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        GUILayout.Box(previewRenderTexture, 
+            GUILayout.Height(256), GUILayout.MaxHeight(256));
+    }
+}
--- Assets/chocopoi/AvatarImage/Scripts/AvatarImageScript.cs
+++ Assets/chocopoi/AvatarImage/Scripts/AvatarImageScript.cs
@@ -1,56 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Core;
+using System.Linq;
 
 public class AvatarImageScript : MonoBehaviour
 {
-    private VRC_AvatarDescriptor m_AvatarDescriptor;
 
-    // Start is called before the first frame update
-    void Start()
+    public VRC_AvatarDescriptor avatar;
+
+    private Canvas avatarImageCanvas;
+
+    private Text versionText;
+
+    private Text randomText;
+
+    private System.Random random;
+
+    private bool hookAttempted = false;
+    
+    void Awake()
     {
-        m_AvatarDescriptor = Core.ApiAvatar
-    }
+        random = new System.Random();
+        avatarImageCanvas = GetComponentInChildren<Canvas>();
 
-    // Update is called once per frame
+        versionText = avatarImageCanvas.transform.Find("VersionText").GetComponent<Text>();
+        randomText = avatarImageCanvas.transform.Find("RandomText").GetComponent<Text>();
+
+        randomText.text = RandomString(8);
+    }
+    
     void Update()
     {
-        
+        if (!hookAttempted && Time.timeSinceLevelLoad > 2)
+        {
+            hookAttempted = true;
+            HookIntoCamera();
+        }
+    }
+
+    private void HookIntoCamera()
+    {
+        GameObject vrcCamObject = GameObject.Find("VRCCam");
+        Camera vrcCam = vrcCamObject?.GetComponent<Camera>();
+
+        if (vrcCamObject == null && vrcCam == null)
+        {
+            Debug.LogError("[AvatarImage] Could not find VRCCam or it does not contain a camera in the running scene!");
+            return;
+        }
+
+        vrcCam.cullingMask = LayerMask.NameToLayer("Everything");
+
+        avatarImageCanvas.worldCamera = vrcCam;
+
+        GameObject imageUploadToggleObject = GameObject.Find("ImageUploadToggle");
+
+        if (imageUploadToggleObject == null)
+        {
+            Debug.LogError("[AvatarImage] Could not find image upload toggle in the running scene!");
+            return;
+        }
+        else
+        {
+            Toggle toggle = imageUploadToggleObject.GetComponent<Toggle>();
+            toggle.isOn = true;
+        }
+
+        ObtainApiAvatar();
     }
 
     private void ObtainApiAvatar()
     {
-        Core.PipelineManager pm = avatar.GetComponent<Core.PipelineManager>();
-        if (pm != null && !string.IsNullOrEmpty(pm.blueprintId))
+        PipelineManager pm = avatar.GetComponent<PipelineManager>();
+
+        if (pm != null && !string.IsNullOrEmpty(pm.blueprintId) && avatar.apiAvatar == null)
         {
-            if (avatar.apiAvatar == null)
-            {
-                Core.ApiAvatar av = Core.API.FromCacheOrNew<Core.ApiAvatar>(pm.blueprintId);
-                av.Fetch(
-                    c => avatar.apiAvatar = c.Model as Core.ApiAvatar,
-                    c =>
-                    {
-                        if (c.Code == 404)
-                        {
-                            Core.Logger.Log(
-                                $"Could not load avatar {pm.blueprintId} because it didn't exist.",
-                                Core.DebugLevel.API);
-                            Core.ApiCache.Invalidate<Core.ApiWorld>(pm.blueprintId);
-                        }
-                        else
-                            Debug.LogErrorFormat("Could not load avatar {0} because {1}", pm.blueprintId, c.Error);
-                    });
-                avatar.apiAvatar = av;
-            }
+            ApiAvatar av = API.FromCacheOrNew<ApiAvatar>(pm.blueprintId);
+            av.Fetch(
+                c => avatar.apiAvatar = c.Model as ApiAvatar,
+                c => {
+                    Debug.LogErrorFormat("The avatar with blueprint ID {0} cannot be loaded with error {1}", pm.blueprintId, c.Error);
+                }
+            );
+            avatar.apiAvatar = av;
         }
 
         if (avatar.apiAvatar != null)
         {
-            Core.ApiAvatar a = (avatar.apiAvatar as Core.ApiAvatar);
-            DrawContentInfoForAvatar(a);
-            VRCSdkControlPanel.DrawContentPlatformSupport(a);
+            ApiAvatar a = avatar.apiAvatar as ApiAvatar;
+            versionText.text = a.version.ToString();
+        } else
+        {
+            versionText.text = "1";
         }
+    }
+
+    public string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
