70919391ef12ed0584b49406fc2a8fdba4e60a7f
poi-vrc <77053052+poi-vrc@users.noreply.github.com>
2021-05-13 22:06:44 +0800

    Add implementations

--- Assets/chocopoi/AvatarImage/Editor/AvatarImageEditor.cs
+++ Assets/chocopoi/AvatarImage/Editor/AvatarImageEditor.cs
@@ -2,27 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using System.IO;
 
-[CustomEditor(typeof(AvatarImageScript))]
+[CustomEditor(typeof(ChocopoiAvatarImageCreator))]
 public class AvatarImageEditor : Editor
 {
     private Texture previewRenderTexture;
 
     private Camera previewCamera;
 
+    private string toolVersion = null;
+
     void Awake()
     {
-        GameObject previewCameraObject = GameObject.Find("AvatarImagePreviewCamera");
-        previewCamera = previewCameraObject.GetComponent<Camera>();
+        string objectName = Application.isPlaying ?
+            "VRCCam" :
+            "AvatarImagePreviewCamera";
 
+        GameObject previewCameraObject = GameObject.Find(objectName);
+
+        if (previewCameraObject == null)
+        {
+            Debug.LogErrorFormat("[AvatarImage] Could not find camera \"{0}\" in the running scenes! Cannot initialize preview in inspector!.");
+            return;
+        }
+
+        previewCamera = previewCameraObject.GetComponent<Camera>();
         previewRenderTexture = previewCamera.targetTexture;
+
+        if (toolVersion == null)
+        {
+            toolVersion = GetToolVersionFromFile();
+        }
+    }
+
+    private string GetToolVersionFromFile()
+    {
+        StreamReader reader = new StreamReader("Assets/chocopoi/AvatarImage/version.txt");
+        string str = reader.ReadToEnd();
+        reader.Close();
+        return str;
     }
 
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
-        
+        ChocopoiAvatarImageCreator creator = (ChocopoiAvatarImageCreator)target;
+
+        EditorGUILayout.HelpBox("This avatar image editor script helps you to generate an new image with a version code and timestamp every time you upload your avatar.", MessageType.Info);
+
+        // Obtain the scene objects
+        Transform avatarImageCanvas = creator.transform.Find("AvatarImageCanvas");
+        RawImage customRawImage = avatarImageCanvas?.transform.Find("CustomImage")?.GetComponent<RawImage>();
+        Text versionText = avatarImageCanvas?.transform.Find("VersionText").GetComponent<Text>();
+        Text randomText = avatarImageCanvas?.transform.Find("RandomText").GetComponent<Text>();
+
+        if (avatarImageCanvas == null || customRawImage == null || versionText == null || randomText == null)
+        {
+            EditorGUILayout.HelpBox("Error: AvatarImageCanvas/CustomImage/VersionText/RandomText could not be found in the scenes. Do not change their GameObject names or delete them. You will experience run-time errors when you are trying to upload.", MessageType.Error);
+        }
+
+        GUILayout.Label("Select the avatar that you are going to generate the image for:", EditorStyles.boldLabel);
+        creator.avatar = (VRC_AvatarDescriptor)EditorGUILayout.ObjectField("Avatar", creator.avatar, typeof(VRC_AvatarDescriptor), true);
+
+        // Settings
+        GUILayout.Label("Settings", EditorStyles.boldLabel);
+
+        creator.moveVrcCamToPreviewCameraLocation = GUILayout.Toggle(creator.moveVrcCamToPreviewCameraLocation, "Move VRCCam to the current AvatarImagePreviewCamera location");
+        creator.displayVersionText = GUILayout.Toggle(creator.displayVersionText, "Display the avatar version on the image");
+        creator.displayRandomText = GUILayout.Toggle(creator.displayRandomText, "Display a random text on the image");
+        creator.randomTextLength = EditorGUILayout.IntField("Length of the random text", creator.randomTextLength);
+        creator.displayCustomImage = GUILayout.Toggle(creator.displayCustomImage, "Display custom image");
+        creator.customImageTexture = (Texture)EditorGUILayout.ObjectField("Custom Image Texture", creator.customImageTexture, typeof(Texture), true);
+
+        // Miscellaneous
+        GUILayout.Label("Miscellaneous", EditorStyles.boldLabel);
+        creator.autoTickUploadImageEveryUpload = GUILayout.Toggle(creator.autoTickUploadImageEveryUpload, "Auto tick \"Upload Image\" on every upload");
+        creator.autoTickAgreement = GUILayout.Toggle(creator.autoTickAgreement, "Auto tick the avatar upload agreement");
+        creator.autoUpload = GUILayout.Toggle(creator.autoUpload, "Auto upload (without clicking the \"Upload\" button manually)");
+
+        // Update the preview image
+        if (customRawImage != null)
+        {
+            versionText.gameObject.SetActive(creator.displayVersionText);
+            randomText.gameObject.SetActive(creator.displayRandomText);
+            customRawImage.gameObject.SetActive(creator.displayCustomImage);
+            customRawImage.texture = creator.customImageTexture;
+            EditorUtility.SetDirty(target);
+        }
+
+        GUILayout.Label("Preview", EditorStyles.boldLabel);
         GUILayout.Box(previewRenderTexture, 
             GUILayout.Height(256), GUILayout.MaxHeight(256));
+
+        EditorGUILayout.Separator();
+        GUILayout.Label("Tool Version: " + toolVersion);
+        EditorGUILayout.SelectableLabel("https://github.com/poi-vrc/AvatarImage");
     }
 }
---
+++ Assets/chocopoi/AvatarImage/Scripts/ChocopoiAvatarImageCreator.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Core;
+using System.Linq;
+
+public class ChocopoiAvatarImageCreator : MonoBehaviour
+{
+
+    public VRC_AvatarDescriptor avatar;
+
+    public bool displayVersionText = true;
+
+    public bool displayRandomText = true;
+
+    public bool displayCustomImage = false;
+
+    public bool moveVrcCamToPreviewCameraLocation = true;
+
+    public bool autoTickUploadImageEveryUpload = true;
+
+    public bool autoTickAgreement = true;
+
+    public bool autoUpload = false;
+
+    public int randomTextLength = 8;
+
+    public Texture customImageTexture;
+
+    private Canvas avatarImageCanvas;
+
+    private Camera previewCamera;
+
+    private RawImage customImage;
+
+    private Text versionText;
+
+    private Text randomText;
+
+    private System.Random random;
+
+    private bool hookAttempted = false;
+    
+    void Awake()
+    {
+        random = new System.Random();
+        avatarImageCanvas = GetComponentInChildren<Canvas>();
+        versionText = avatarImageCanvas.transform.Find("VersionText")?.GetComponent<Text>();
+        randomText = avatarImageCanvas.transform.Find("RandomText")?.GetComponent<Text>();
+        customImage = avatarImageCanvas.transform.Find("CustomImage")?.GetComponent<RawImage>();
+        previewCamera = GameObject.Find("AvatarImagePreviewCamera")?.GetComponent<Camera>();
+
+        randomText.text = RandomString(randomTextLength);
+
+        versionText.gameObject.SetActive(displayVersionText);
+        randomText.gameObject.SetActive(displayRandomText);
+        customImage.gameObject.SetActive(displayCustomImage);
+        customImage.texture = customImageTexture;
+    }
+    
+    void Update()
+    {
+        if (!hookAttempted && Time.timeSinceLevelLoad > 2)
+        {
+            hookAttempted = true;
+            HookIntoCamera();
+        }
+    }
+
+    private void HookIntoCamera()
+    {
+        GameObject vrcCamObject = GameObject.Find("VRCCam");
+        Camera vrcCam = vrcCamObject?.GetComponent<Camera>();
+
+        if (vrcCamObject == null && vrcCam == null)
+        {
+            Debug.LogError("[AvatarImage] Could not find VRCCam or it does not contain a camera in the running scene!");
+            return;
+        }
+
+        vrcCam.cullingMask = LayerMask.NameToLayer("Everything");
+
+        if (moveVrcCamToPreviewCameraLocation)
+        {
+            vrcCam.transform.position = previewCamera.transform.position;
+            vrcCam.transform.rotation = previewCamera.transform.rotation;
+            vrcCam.transform.localScale = previewCamera.transform.localScale;
+        }
+
+        avatarImageCanvas.worldCamera = vrcCam;
+
+        bool firstTimeUpload = true;
+
+        if (autoTickUploadImageEveryUpload)
+        {
+            GameObject imageUploadToggleObject = GameObject.Find("ImageUploadToggle");
+
+            if (imageUploadToggleObject == null)
+            {
+                Debug.LogError("[AvatarImage] Could not find image upload toggle in the running scene!");
+                return;
+            }
+            else
+            {
+                Toggle toggle = imageUploadToggleObject.GetComponent<Toggle>();
+
+                firstTimeUpload = toggle.isOn;
+
+                toggle.isOn = true;
+                toggle.onValueChanged?.Invoke(true);
+            }
+        }
+
+        if (autoUpload || autoTickAgreement)
+        {
+            GameObject toggleWarrantObject = GameObject.Find("ToggleWarrant");
+
+            if (toggleWarrantObject == null)
+            {
+                Debug.LogError("[AvatarImage] Could not find warrant toggle in the running scene!");
+                return;
+            }
+            else
+            {
+                Toggle toggle = toggleWarrantObject.GetComponent<Toggle>();
+                toggle.isOn = true;
+                toggle.onValueChanged?.Invoke(true);
+            }
+
+            if (autoUpload && !firstTimeUpload)
+            {
+                GameObject uploadButtonObject = GameObject.Find("UploadButton");
+
+                if (uploadButtonObject == null)
+                {
+                    Debug.LogError("[AvatarImage] Could not find upload button in the running scene!");
+                    return;
+                }
+                else
+                {
+                    Button button = uploadButtonObject.GetComponent<Button>();
+                    button.onClick?.Invoke();
+                }
+            }
+        }
+
+        ObtainApiAvatar();
+    }
+
+    private void ObtainApiAvatar()
+    {
+        PipelineManager pm = avatar.GetComponent<PipelineManager>();
+
+        if (pm != null && !string.IsNullOrEmpty(pm.blueprintId) && avatar.apiAvatar == null)
+        {
+            ApiAvatar av = API.FromCacheOrNew<ApiAvatar>(pm.blueprintId);
+            av.Fetch(
+                c => avatar.apiAvatar = c.Model as ApiAvatar,
+                c => {
+                    Debug.LogErrorFormat("The avatar with blueprint ID {0} cannot be loaded with error {1}", pm.blueprintId, c.Error);
+                }
+            );
+            avatar.apiAvatar = av;
+        }
+
+        if (avatar.apiAvatar != null)
+        {
+            ApiAvatar a = avatar.apiAvatar as ApiAvatar;
+            versionText.text = a.version.ToString();
+        } else
+        {
+            versionText.text = "1";
+        }
+    }
+
+    public string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
